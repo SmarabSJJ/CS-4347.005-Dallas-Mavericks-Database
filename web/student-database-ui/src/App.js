661f9587a9250c7042fe7521cef5267752e1b313
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2024-11-07 14:34:11 -0600

    Added Project Skeleton Files such as Menu and Content

--- web/student-database-ui/src/App.js
+++ web/student-database-ui/src/App.js
@@ -1,9 +1,13 @@
 import Logo from "./components/Logo";
+import Menu from "./components/Menu";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
       <Logo/>
+      <Menu/>
+      <Content/>
     </div>
   );
 }
