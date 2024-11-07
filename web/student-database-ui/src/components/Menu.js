2f126675cdbb30950491e477e72c8c82d49aba96
Aarez Ali Momin <aarez.umatiya111@gmail.com>
2024-11-07 17:31:59 -0600

    aarez first commit

--- web/student-database-ui/src/components/Menu.js
+++ web/student-database-ui/src/components/Menu.js
@@ -1,7 +1,16 @@
+import './menu.css';
+
 const Menu = () => {
   return ( 
     <div className="menu">
 
+      <div className="buttons" >
+        <a className='query'> Query </a>
+        <a className='insert'> Insert </a>
+        <a className='update'> Update </a>
+        <a className='delete'> Delete </a>
+        <a className='exit'> Exit </a>
+      </div>
     </div>
   );
 }
