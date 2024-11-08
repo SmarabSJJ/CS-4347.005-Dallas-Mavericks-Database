a8a74b228376d1a8e36164d2af04e3427fe47613
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2024-11-08 17:26:36 -0600

    Changed up the styling for the Menu

--- web/student-database-ui/src/components/Menu.js
+++ web/student-database-ui/src/components/Menu.js
@@ -1,17 +1,13 @@
-import './menu.css';
 
 const Menu = () => {
   return ( 
     <div className="menu">
-
       <div className="buttons" >
-        <nav>
         <a className='query' href='#'> Query </a>
         <a className='insert' href='#'> Insert </a>
         <a className='update' href='#'> Update </a>
         <a className='delete' href='#'> Delete </a>
         <a className='exit' href='#'> Exit </a>
-        </nav>
       </div>
     </div>
   );
