7fb6ab529f3a16bcb561f9876a528383654ed501
Aarez Ali Momin <aarez.umatiya111@gmail.com>
2024-11-09 11:47:48 -0600

    removed menu.css and put all css in index

--- web/student-database-ui/src/components/Menu.js
+++ web/student-database-ui/src/components/Menu.js
@@ -1,17 +1,12 @@
-import './menu.css';
-
 const Menu = () => {
   return ( 
     <div className="menu">
-
       <div className="buttons" >
-        <nav>
-        <a className='query' href='#'> Query </a>
-        <a className='insert' href='#'> Insert </a>
-        <a className='update' href='#'> Update </a>
-        <a className='delete' href='#'> Delete </a>
-        <a className='exit' href='#'> Exit </a>
-        </nav>
+        <button className='query' href='#'> Query </button>
+        <button className='insert' href='#'> Insert </button>
+        <button className='update' href='#'> Update </button>
+        <button className='delete' href='#'> Delete </button>
+        <button className='exit' href='#'> Exit </button>
       </div>
     </div>
   );
