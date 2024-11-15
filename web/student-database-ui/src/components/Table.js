5f4801b15a945edbebec617189c7bb52119bdcfe
Aarez Ali Momin <aarez.umatiya111@gmail.com>
2024-11-14 19:30:20 -0600

    table made

--- web/student-database-ui/src/components/Table.js
+++ web/student-database-ui/src/components/Table.js
@@ -1,7 +1,32 @@
+import axios from "axios";
+import { useEffect } from "react";
 const Table = () => {
-  return ( 
+  return (
+    <div>  
+    <h2 className="tableh1"> Table </h2>
     <div className="table">
-      
+        <table>
+          <tr>
+            <th> Id </th>
+            <th> Name </th>
+            <th> JerseyNum </th>
+          </tr>
+          <tr>
+            <td> 12345 </td>
+            <td> Tyson </td>
+            <td> 10 </td>
+          </tr>
+          <tr>
+            <td> 52454 </td>
+            <td> Paul </td>
+            <td> 20 </td>
+          </tr>
+            <td> 32554 </td>
+            <td> Kyrie </td>
+            <td> 30 </td>
+        </table>
+    </div>
+    
     </div>
    );
 }
