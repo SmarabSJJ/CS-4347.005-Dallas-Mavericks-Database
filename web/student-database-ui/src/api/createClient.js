830c4ff3babca210f71237a7a78fd5ed928620eb
Aarez Ali Momin <154018654+amomin2005@users.noreply.github.com>
2025-06-02 11:46:13 -0500

    Update createClient.js

--- web/student-database-ui/src/api/createClient.js
+++ web/student-database-ui/src/api/createClient.js
@@ -1,6 +1,1 @@
 import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://ohzvjybrwhefxpuzpwnd.supabase.co/",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oenZqeWJyd2hlZnhwdXpwd25kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwMDYxOTksImV4cCI6MjA0NjU4MjE5OX0.bpXWYho9237KxCQBnPDI7wAoE1PvoOu7grDDjuW4VPQ"
-)
